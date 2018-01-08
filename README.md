d7772c5e10434678d88c91fc5f9a432d18fac7e0
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-09 02:01:05 +0300

    Script is created

---
+++ README.md
@@ -0,0 +1,18 @@
+# Script to register on revet
+
+This is an automated test for registration by email on https://dev2.revetinc.com/
+
+# Preconditions
+
+* [Python](https://www.python.org/) 3+
+* Latest version of [geckodriver](https://github.com/mozilla/geckodriver/releases) for Firefox
+* Latest [Firefox](https://www.mozilla.org) web browser
+* Latest version of [selenium](https://pypi.python.org/pypi/selenium) web driver for Python
+
+# How to start
+
+Example of script launch on Linux, Python 3.5:
+```
+$ python onboarding_revert.py
+```
+
---
+++ onboarding_revet.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import time
+
+wd = webdriver.Firefox()
+
+
+def registration():
+    wd.get("https://dev2.revetinc.com/")
+    wd.find_element_by_css_selector("a[href='/join-now/simple']").click()
+    wd.find_element_by_css_selector("a[href='/join-now']").click()
+    wd.find_element_by_css_selector("input[name='FirstName']").send_keys('QA')
+    wd.find_element_by_css_selector("input[name='LastName']").send_keys('Engineer')
+    wd.find_element_by_css_selector("input[name='UserName']").send_keys('test@example.com')
+    wd.find_element_by_css_selector("input[name='Password']").send_keys('Qwerty1@')
+    wd.find_element_by_css_selector("button[type='submit']").click()
+    time.sleep(3)
+    wd.close()
+
+
+registration()
